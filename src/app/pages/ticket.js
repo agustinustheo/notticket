b09dbc939c69d2bfb866f6187ae81ac1406eb25d
Arya S <arya.surya021@gmail.com>
2020-10-09 21:44:12 +0700

    Restructuring Project Folder & Finished Routing

---
+++ src/app/pages/ticket.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TicketPage extends Component {
+    render(){
+        return(
+            <div>
+                Ticket
+            </div>
+        )
+    }
+}
