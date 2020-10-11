ad260afe83360bf535da399a3238ace447bb3645
arvin.sulimto <arvin.sulimto@binus.edu>
2020-10-11 13:11:35 +0700

    Fix CSS Ticket to Responsive

--- src/app/pages/ticket.js
+++ src/app/pages/ticket.js
@@ -3,105 +3,94 @@ import styled from 'styled-components'
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
 import { BaseURL } from '../constant/variables'
 import Loader from "../pages/components/loader";
-
-const Flex = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    flex:3;
-    width:80%;
-    margin: 0 auto;
-    display:flex;
-    height: 40rem;
-    align-items:center;
-    justify-content: center;   
-`
+import styles from '../../app/style.css'
 
 const OneThirdFlex = styled.div`
-    flex:1;
-    height: 100%;
-    align-items:center;
-    flex-direction: column;
-`
-
-const TwoThirdFlex = styled.div`
-    position:relative;
-    flex:2;
-    height: 100%;
-    display:flex;
-    flex-direction: column;
-    perspective: 4rem;
+color:white;
 `
 
 const Title = styled.h2`
-    font-size: 8rem;
+font-size: 8rem;
 `
 
 const SubTitle = styled.h4`
-    font-size: 3rem;
-    display: inline-block;
+font-size: 3rem;
+
+`
+const LinkButton = styled.button`
+border: solid 2px #FFC20F;
+background:black;
+color:white;
+padding:2rem;
+border-radius:10px;
+font-size:2rem;
+margin-top:3rem;
+transition: all .5s;
+&:hover {
+    background: #FFC20F;
+    color: black;
+}
 `
 
+const TwoThirdFlex = styled.div`
+position:relative;
+flex:2;
+height: 100%;
+display:flex;
+flex-direction: column;
+perspective: 4rem;
+`
+
+
 const PureFlex = styled.div`
-    display:flex;
-    align-items:center;   
+display:inline-block;
+align-items:center;   
 `
 
 const LeftBorderedFlex = styled.div`
-    display:flex;
-    align-items:center;   
-    border-left: dashed .4rem #FFC20F;
+width:100%; 
+border-left: dashed .4rem #FFC20F;
 `
 
 const YFlex = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: space-between;
+display:inline;
 `
 
 const TicketContainer = styled.div`
-    position:absolute;
-    display: flex;
-    background:black;
-    box-shadow: 0 0 2rem #FFC20F;
-    justify-content: space-between;
-    top:50%;
-    left: 50%;
-    transform:translate(-50%, -50%);
-    width:90%;
-    height:80%;
-    border: solid 2px #FFC20F;
-    border-radius:3rem;
-    transition: all .5s;
+display:block;
+background:black;
+width:90%;
+height:80%;
+border: solid 2px #FFC20F;
+        border-radius:3rem;
+        transition: all .5s;
+        box-shadow: 0 0 2rem #FFC20F;
 `
 
 const Avatar = styled.div`
-    display:inline-block;
-    margin-top:2rem;
-    margin-left: 2rem;
-    border-radius: 50%;
-    width:12rem;
-    height:12rem;
-
-    & > img {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-    }
+display:inline-block;
+margin-top:2rem;
+margin-left: 2rem;
+border-radius: 50%;
+width:12rem;
+height:12rem;
+& > img {
+border-radius: 50%;
+}
 `
 
 const XFlex = styled.div`
-    display:flex;
-    flex-direction: row;
-    width: 100%;
-    justify-content: space-around;
-    margin-left: 3%;
-    transform:translateY(-10%);
+width:100%;
 `
 
 const SmallTitle = styled.h2`
     font-size:3rem;
+`
+
+const Line = styled.div`
+border-top: dashed 2px #FFC20F;
+width:100%; 
+
 `
 
 const SmallSubtitle = styled.h4`
@@ -109,66 +98,29 @@ const SmallSubtitle = styled.h4`
 `
 
 const LeftSpace = styled.div`
+    display:inline-block;
     margin-left:2.5rem;
 `
-
-const Background = styled.div`
-    height:100vh;
-    width:100vw;
-    overflow-y:hidden;
-    color: white;
-`
-
-const Rotated = styled.h2`
-    transform: rotate(90deg);
-    font-size:3rem;
-`
-
 const Button = styled.div`
-    display:flex;
-    align-items:center;
     border: solid 2px #FFC20F;
     margin:1rem;
     padding: 1rem;
+    display: inline-block;
     background:black;
     text-align:center;
     font-size:2rem;
     border-radius:10px;
     transition: all .5s;
-
     &:hover {
         background: #FFC20F;
         color: black;
         cursor: pointer;
     }
 `
-
-const LinkButton = styled.button`
-    border: solid 2px #FFC20F;
-    background:black;
-    color:white;
-    padding:2rem;
-    border-radius:10px;
+const Rotated = styled.h2`
+    text-align:center;
     font-size:2rem;
-    margin-top:3rem;
-    transition: all .5s;
-
-    &:hover {
-        background: #FFC20F;
-        color: black;
-    }
 `
-
-const LoadingContainer = styled.div`
-    width:100%;
-    display:flex;
-    justify-content:center;
-    height: 100%;
-    align-items: center;
-    z-index: 99;
-    position: fixed;
-`
-
 
 export default class TicketPage extends Component {
     
@@ -218,17 +170,20 @@ export default class TicketPage extends Component {
     }
 
     render(){
+      
         return(
-            this.state.isLoading ?  <LoadingContainer><Loader/></LoadingContainer> :
-            <Background>
-                <Flex>
-                    <OneThirdFlex>
+            <div className="App">
+            <div className="row">
+            <div className="half-row">
+            <OneThirdFlex>
                         <Title>You're In.</Title>
                         <br/>
                         <SubTitle>Share Your Ticket to Social Media to jazz up the concert!</SubTitle>
                         <LinkButton>COPY TICKET URL</LinkButton>
                     </OneThirdFlex>
-                    <TwoThirdFlex ref={this.parentRef}>
+            </div>
+
+                    <div className="half-row0">
                         <TicketContainer ref={this.childRef}>
                             <YFlex>
                                 <PureFlex>
@@ -246,17 +201,22 @@ export default class TicketPage extends Component {
                                         <SmallSubtitle>Date  : {this.state.eventDate}</SmallSubtitle>
                                     </LeftSpace>
                                 </YFlex>
+                     
                                 <XFlex>
+                                    <br></br>
+                                    <Line></Line>
+                                    <Rotated>No 085782523656</Rotated>
+                                    <Line></Line>
+                                    <br></br>
                                     <Button><FaFacebook/>&nbsp;&nbsp;Facebook</Button>
                                     <Button><FaInstagram/>&nbsp;&nbsp;Instagram</Button>
                                     <Button><FaTwitter/>&nbsp;&nbsp;Tweet</Button>   
                                 </XFlex>
                             </YFlex>
-                            <LeftBorderedFlex><Rotated>No {this.state.eventID.substring(0,7)}</Rotated></LeftBorderedFlex>
                         </TicketContainer>
-                    </TwoThirdFlex>
-                </Flex>
-            </Background>
+                        </div>
+          </div>
+          </div>
         )
     }
 }
